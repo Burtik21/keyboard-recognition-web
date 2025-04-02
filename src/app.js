ae3aa929680fed00f6c74ce8f0bd774c331d9dd0
Burtik21 <simon.stopa@gmail.com>
2025-04-02 22:21:07 +0200

    update

---
+++ src/app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+const audioRoutes = require('./routes/audioRoutes');
+require('dotenv').config();
+//const sequelize = require('./config/db');
+
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// Nastavení šablonovacího enginu EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Statické soubory (CSS, JS, obrázky)
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false },
+    cookie: { maxAge: 1000 * 60 * 60 }
+}));
+
+
+// Hlavní stránkas
+app.use('/', audioRoutes);
+app.use('/audio', audioRoutes);
+//app.use("/auth", authRoutes)
+
+
+module.exports = app;
+
+// Spuštění serveru
--- src/server.js
+++ src/server.js
@@ -1,35 +1,17 @@
-const express = require('express');
-const multer = require('multer');
-const fs = require('fs');
-const path = require('path');
+const app = require('./app');
+const http = require('http');
+const WebSocket = require('ws');
+const webrtcController = require('./controllers/webrtcController');
+require('dotenv').config();
 
-const app = express();
-const PORT = 5500;
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
-// Statické soubory budou dostupné na /keyboard/
-app.use('/keyboard', express.static('public'));
-
-// Ujisti se, že složka uploads existuje
-if (!fs.existsSync('uploads')) {
-    fs.mkdirSync('uploads');
-}
-
-let counter = 1;
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => cb(null, 'uploads'),
-    filename: (req, file, cb) => {
-        const filename = `nahravka_${counter}.webm`;
-        counter++;
-        cb(null, filename);
-    }
-});
-const upload = multer({ storage });
-
-// Upload endpoint pod /keyboard/upload
-app.post('/keyboard/upload', upload.single('audio'), (req, res) => {
-    res.send({ message: 'Nahrávka uložena.' });
+wss.on('connection', (socket) => {
+    webrtcController.handleSocket(socket); // deleguj logiku do controlleru
 });
 
-app.listen(PORT, () => {
-    console.log(`Server běží na http://localhost:${PORT}/keyboard`);
+const PORT = process.env.PORT || 5500;
+server.listen(PORT, () => {
+    console.log(`✅ Server běží na http://localhost:${PORT}`);
 });
