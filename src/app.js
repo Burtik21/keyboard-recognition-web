702f039b648b14c6ff7fe688e5cc979a90156d20
Burtik21 <simon.stopa@gmail.com>
2025-04-03 10:58:35 +0200

    update

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 // Statické soubory (CSS, JS, obrázky)
-app.get('/js/webrtcClient.js', (req, res) => {
+app.get('/keyboard/js/webrtcClient.js', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'js', 'webrtcClient.js'));
 });
 
