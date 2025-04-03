cf89824c3e7344078fef8a2eb6ac9dc9b851fd24
Burtik21 <simon.stopa@gmail.com>
2025-04-03 11:06:52 +0200

    update

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 // Statické soubory (CSS, JS, obrázky)
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use(session({
